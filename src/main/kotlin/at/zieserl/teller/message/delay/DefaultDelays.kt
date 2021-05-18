54c6fdd77cab4b0046ea1f18ce19988e63ce2acb
Luca Zieserl <luczies@gmail.com>
2021-05-18 16:20:17 +0200

    add default delays

---
+++ src/main/kotlin/at/zieserl/teller/message/delay/DefaultDelays.kt
@@ -0,0 +1,9 @@
+package at.zieserl.teller.message.delay
+
+interface DefaultDelays {
+    companion object {
+        const val FIVE_SECONDS = 5000
+        const val THREE_SECONDS = 3000
+        const val ONE_SECOND = 1000
+    }
+}
