cfe3121519f357f8bbb10f8dcf4c0e3c5f43c091
Luca Zieserl <luczies@gmail.com>
2021-05-18 17:54:03 +0200

    change defaultdelay types to long

--- src/main/kotlin/at/zieserl/teller/message/delay/DefaultDelays.kt
+++ src/main/kotlin/at/zieserl/teller/message/delay/DefaultDelays.kt
@@ -2,8 +2,8 @@ package at.zieserl.teller.message.delay
 
 interface DefaultDelays {
     companion object {
-        const val FIVE_SECONDS = 5000
-        const val THREE_SECONDS = 3000
-        const val ONE_SECOND = 1000
+        const val FIVE_SECONDS: Long = 5000
+        const val THREE_SECONDS: Long = 3000
+        const val ONE_SECOND: Long = 1000
     }
 }
