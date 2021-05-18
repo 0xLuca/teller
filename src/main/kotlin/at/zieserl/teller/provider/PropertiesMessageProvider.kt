51ad906ccd1426c901d3acce8675405293838d98
Luca Zieserl <luczies@gmail.com>
2021-05-18 14:03:13 +0200

    add chatcolor char translation

--- src/main/kotlin/at/zieserl/teller/provider/PropertiesMessageProvider.kt
+++ src/main/kotlin/at/zieserl/teller/provider/PropertiesMessageProvider.kt
@@ -1,13 +1,14 @@
 package at.zieserl.teller.provider
 
 import at.zieserl.teller.message.Message
+import org.bukkit.ChatColor
 import org.bukkit.plugin.java.JavaPlugin
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.*
 
 class PropertiesMessageProvider(private val properties: Properties) : MessageProvider {
-    override fun provide(key: String): Message = Message(this, properties.getProperty(key, "Invalid message key $key!"))
+    override fun provide(key: String): Message = Message(this, ChatColor.translateAlternateColorCodes('&', properties.getProperty(key, "Invalid message key $key!")))
 
     companion object {
         fun fromPlugin(plugin: JavaPlugin, resourcePath: String) : PropertiesMessageProvider {
